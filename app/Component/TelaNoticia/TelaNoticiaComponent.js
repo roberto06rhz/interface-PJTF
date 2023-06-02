fcbb437340801388a7dc211b088b7c10db20370d
Jeremias Verissimo Gomes <68084107+ojeremias@users.noreply.github.com>
2023-06-02 14:50:39 -0300

    implementando busca de deputados federais

--- app/Component/TelaNoticia/TelaNoticiaComponent.js
+++ app/Component/TelaNoticia/TelaNoticiaComponent.js
@@ -4,31 +4,60 @@ import { Avatar, List, TextInput } from 'react-native-paper';
 
 const TelaNoticiaComponent = () => {
   const [politicos, setPoliticos] = useState([]);
-   const [text, setText] = React.useState("");
+  const [politicoFiltrado, setPoliticoFiltrado] = useState([])
+  const [text, setText] = useState("");
+
+  useEffect(() => {
 
   const getPoliticos = async () => {
     try {
       const response = await fetch("https://dadosabertos.camara.leg.br/api/v2/deputados?ordem=ASC&ordenarPor=nome");
       const json = await response.json();
-      console.log(json)
       setPoliticos  (json.dados);
     } catch (error) {
       console.error(error);
     }
   };
-
-  useEffect(() => {
     getPoliticos();
   }, []);
 
-  return (
+  useEffect(()=>{
+    const nomesPoliticos = politicos.map(politico => {
+     const obj = {
+      nome: politico.nome,
+       id: politico.id
+     } 
 
-      <View style={{ flex: 1, padding: 24 }}>
-          <TextInput
-            label="Digite o nome do deputado"
-            value={text}
-            onChangeText={text => setText(text)}
-          />
+     return obj
+      
+    })
+    const idsPoliticos = politicos.map(politico => politico.id)
+
+    const nomeBuscado = politicos.filter((item) =>
+    item.nome.toLowerCase().includes(text.toLocaleLowerCase()))
+
+    setPoliticoFiltrado(nomeBuscado)
+
+  },[text])
+
+
+  const ComponentFiltrados = ({data}) => {
+    console.log(politicoFiltrado)
+    if(politicoFiltrado.length > 0){
+        return (
+     <FlatList
+          data={politicoFiltrado}
+          keyExtractor={({ id }) => id}
+          renderItem={({ item }) => (
+            <List.Item title={item.nome} 
+            description={item.siglaPartido} 
+            left={props => <Avatar.Image source={{uri: item.urlFoto}} />} />
+          )}
+        />
+      
+    ) 
+    }else{
+      return(
         <FlatList
           data={politicos}
           keyExtractor={({ id }) => id}
@@ -38,9 +67,25 @@ const TelaNoticiaComponent = () => {
             left={props => <Avatar.Image source={{uri: item.urlFoto}} />} />
           )}
         />
+      )
+    }
+  }
+  
+  return (
+
+      <View style={{ flex: 1, padding: 24 }}>
+          <TextInput
+            outlineColor='green'
+            label="Nome do deputado federal"
+            value={text}
+            onChangeText={text => setText(text)}
+          />
+
+    <ComponentFiltrados /> 
+          
+      
       </View>
 
   );
 };
-
 export default TelaNoticiaComponent;

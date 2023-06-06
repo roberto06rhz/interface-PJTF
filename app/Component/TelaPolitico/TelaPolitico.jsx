ebeaba8a2d6e48055de21a4ba68d3d6f54dc3913
Pedrix55 <pedrogabriel12gamer@outlook.com>
2023-06-06 11:42:55 -0300

    faz o L

--- app/Component/TelaPolitico/TelaPolitico.jsx
+++ app/Component/TelaPolitico/TelaPolitico.jsx
@@ -1,12 +1,38 @@
-import React from "react";
-import {Text} from 'react-native'
+import React, { useState } from "react";
+import {Text, Image, StyleSheet, View} from 'react-native'
 
 const TelaPolitico = ({ route }) => {
-  console.log(route.params.chaveValor)
+//  console.log(route.params.chaveValor)
   const dadosPolitico = route.params.chaveValor
-  return <>
-    <Text>Faz o L</Text>
+  console.log(dadosPolitico)
 
-  </>;
+  return(
+  <View style={{flex: 1}}>
+    <View style={{height: 300, width: 200}}>
+      <Image
+  
+      style={style.image}
+        source={{uri:`${dadosPolitico.urlFoto}`}}
+      />
+      <View style={style.content}>
+
+        <Text>{dadosPolitico.nome}</Text>
+
+      </View>
+
+    </View>
+
+  </View>
+
+  ) 
 };
+
+const style = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: '100%',
+    objectFit: 'contain'
+  }
+  
+});
 export default TelaPolitico;

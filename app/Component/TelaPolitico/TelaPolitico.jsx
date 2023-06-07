b652a77e16a49e1dd2150c2d1dc1cb25a5d943ce
Jeremias Verissimo <jeremias123verissimo@gmail.com>
2023-06-06 23:41:09 -0300

    Novas alterações com ajuda do Mateus Duarte

--- app/Component/TelaPolitico/TelaPolitico.jsx
+++ app/Component/TelaPolitico/TelaPolitico.jsx
@@ -1,38 +1,94 @@
-import React, { useState } from "react";
-import {Text, Image, StyleSheet, View} from 'react-native'
+import React, { useEffect, useState } from "react";
+import { Text, Image, StyleSheet, View, FlatList } from "react-native";
+import { List } from "react-native-paper";
 
 const TelaPolitico = ({ route }) => {
-//  console.log(route.params.chaveValor)
-  const dadosPolitico = route.params.chaveValor
-  console.log(dadosPolitico)
+  //  console.log(route.params.chaveValor)
+  /* console.log(route); */
+  const dadosPolitico = route.params.chaveValor;
+  /* console.log(dadosPolitico); */
+  //console.log(dadosPolitico.uri.dados.gabinete.email);
 
-  return(
-  <View style={{flex: 1}}>
-    <View style={{height: 300, width: 200}}>
-      <Image
-  
-      style={style.image}
-        source={{uri:`${dadosPolitico.urlFoto}`}}
-      />
-      <View style={style.content}>
+  const [data, setData] = useState([]);
+  const [selecionados, setSelecionados] = useState([]);
+  // Substituido pela linha 26 à 40 | Se utilizando de IIFE
+  // const showMoreData = async () => {
+  //   try {
+  //     const response = await fetch(
+  //       `https://dadosabertos.camara.leg.br/api/v2/deputados/${dadosPolitico.id}/despesas`
+  //     );
+  //     const json = await response.json();
+  //     //console.log(json.dados);
+  //     setData(json.dados);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
 
-        <Text>{dadosPolitico.nome}</Text>
+  useEffect(() => {
+    //Utilizando de IIFE -> Immediately Invoked Function Expression
+    (async () => {
+      try {
+        const response = await fetch(
+          `https://dadosabertos.camara.leg.br/api/v2/deputados/${dadosPolitico.id}/despesas`
+        );
+        const json = await response.json();
+        //console.log(json.dados);
+        setData(json.dados);
+      } catch (error) {
+        /* console.error(error); */
+      }
+    })();
+  }, []);
 
+  return (
+    <View style={{ flex: 1 }}>
+      <View style={{ height: 450, width: 414 }}>
+        <Image
+          style={style.image}
+          source={{ uri: `${dadosPolitico.urlFoto}` }}
+        />
+        <View style={style.content}>
+          <Text>Nome: {dadosPolitico.nome}</Text>
+          <Text>Partido: {dadosPolitico.siglaPartido}</Text>
+          <Text>Contado: {dadosPolitico.email}</Text>
+          {/* {data.map((despesa) => (
+            <View style={{ marginBottom: "2rem" }}>
+              <Text>Ano: {despesa.ano}</Text>
+              <Text>Mes: {despesa.mes}</Text>
+              <Text>Tipo: {despesa.tipoDespesa}</Text>
+            </View>
+          ))} */}
+          {/*  <FlatList
+            style={{
+              backgroundColor: "red",
+              color: "red",
+              marginBottom: "15px",
+            }}
+            data={data}
+            keyExtractor={({ codDocumento }) => codDocumento}
+            renderItem={({ despesa }) => {
+              return <Text>Ano: {despesa.ano}</Text>;
+            }} // Renderiza uma View vazia
+          /> */}
+
+          {data.map((despesa, i) => (
+            <View key={i}>
+              <Text>
+                Ano: {despesa.ano} mes: {despesa.mes}
+              </Text>
+            </View>
+          ))}
+        </View>
       </View>
-
     </View>
-
-  </View>
-
-  ) 
+  );
 };
-
 const style = StyleSheet.create({
   image: {
-    width: '100%',
-    height: '100%',
-    objectFit: 'contain'
-  }
-  
+    width: "100%",
+    height: "100%",
+    objectFit: "contain",
+  },
 });
 export default TelaPolitico;
--- app/Component/TelaPoliticos/TelaPolticos.js
+++ app/Component/TelaPoliticos/TelaPolticos.js
@@ -8,7 +8,7 @@ const TelaPoliticos = ({ navigation }) => {
   const [text, setText] = useState("");
 
   useEffect(() => {
-    const getPoliticos = async () => {
+    (async () => {
       try {
         const response = await fetch(
           "https://dadosabertos.camara.leg.br/api/v2/deputados?ordem=ASC&ordenarPor=nome"
@@ -18,8 +18,7 @@ const TelaPoliticos = ({ navigation }) => {
       } catch (error) {
         console.error(error);
       }
-    };
-    getPoliticos();
+    })();
   }, []);
 
   useEffect(() => {
@@ -30,35 +29,36 @@ const TelaPoliticos = ({ navigation }) => {
   }, [text, politicos]);
 
   const ComponentFiltrados = () => {
-    console.log(politicoFiltrado);
-    if (politicoFiltrado.length > 0) {
-      return (
-        <FlatList
-          data={politicoFiltrado}
-          keyExtractor={({ id }) => id}
-          renderItem={({ item }) => (
-            <Pressable onPress={() => {navigation.navigate('telaPolitico', {
-              chaveValor: item
-            })}}>
-              <List.Item
-                title={item.nome}
-                description={item.siglaPartido}
-                left={(props) => <Avatar.Image source={{ uri: item.urlFoto }} />}
-              />
-            </Pressable>
-          )}
-        />
-      );
-    } else {
-      return (
-        <FlatList
-          data={[]}
-          keyExtractor={() => ""}
-          renderItem={() => <View />} // Renderiza uma View vazia
-        />
-      );
-    }
+    if (politicoFiltrado <= 0)
+      <FlatList
+        data={[]}
+        keyExtractor={() => ""}
+        renderItem={() => <View />} // Renderiza uma View vazia
+      />;
+
+    return (
+      <FlatList
+        data={politicoFiltrado}
+        keyExtractor={({ id }) => id}
+        renderItem={({ item }) => (
+          <Pressable
+            onPress={() => {
+              navigation.navigate("telaPolitico", {
+                chaveValor: item,
+              });
+            }}
+          >
+            <List.Item
+              title={item.nome}
+              description={item.siglaPartido}
+              left={(props) => <Avatar.Image source={{ uri: item.urlFoto }} />}
+            />
+          </Pressable>
+        )}
+      />
+    );
   };
+
   return (
     <View style={{ flex: 1, padding: 24 }}>
       <TextInput

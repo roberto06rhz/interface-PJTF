073ac68705084e7167b950d3776cd879abda600e
Pedrix55 <68084107+ojeremias@users.noreply.github.com>
2023-06-07 11:31:16 -0300

    TOMA! IDERLAN

--- app/Component/TelaPolitico/TelaPolitico.jsx
+++ app/Component/TelaPolitico/TelaPolitico.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Text, Image, StyleSheet, View, FlatList } from "react-native";
 import { List } from "react-native-paper";
+import { GASTOS_FAKE } from "../Utils/DataFake";
 
 const TelaPolitico = ({ route }) => {
   //  console.log(route.params.chaveValor)
@@ -11,6 +12,7 @@ const TelaPolitico = ({ route }) => {
 
   const [data, setData] = useState([]);
   const [selecionados, setSelecionados] = useState([]);
+
   // Substituido pela linha 26 à 40 | Se utilizando de IIFE
   // const showMoreData = async () => {
   //   try {
@@ -34,10 +36,15 @@ const TelaPolitico = ({ route }) => {
         );
         const json = await response.json();
         //console.log(json.dados);
-        setData(json.dados);
+        
+        if(json?.dados)
+          setData(json.dados);
+        else{
+          setData(GASTOS_FAKE);
+        }
         
       } catch (error) {
-        console.error(error)
+        console.error('error:' + error)
       }
     })();
   }, []);
@@ -74,7 +81,7 @@ const TelaPolitico = ({ route }) => {
             }} // Renderiza uma View vazia
           /> */}
 
-          {data.map((despesa, i) => (
+          {data?.map((despesa, i) => (
             <View key={i}>
               <Text>
                 Tipo: {despesa.tipoDespesa}

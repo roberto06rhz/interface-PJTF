46c571c0bf7c4c7a9230f61b585a1ee4e6c77901
Jeremias Verissimo Gomes <75376776+roberto06rhz@users.noreply.github.com>
2023-06-14 08:39:23 -0300

    Revert "xxx"
    This reverts commit 2e02f1a208b1243326f1efac3d549e6ce1522680.

--- app/Component/TelaLogin/TelaLoginComponent.js
+++ app/Component/TelaLogin/TelaLoginComponent.js
@@ -1,70 +1,10 @@
 import { useState } from "react";
-<<<<<<< HEAD
 
-=======
->>>>>>> parent of 3c9034ee (tela login pronta)
 import { SafeAreaView, View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { TextInput } from 'react-native-paper'
 
 
-<<<<<<< HEAD
-=======
-const TelaLoginComponent = ({ navigation }) => {
-    const [email, setEmail] = useState('');
-    const [senha, setSenha] = useState('');
-
-    return (
-
-
-
-
-        <View style={estilo.container}>
-
-
-            <Text style={estilo.titulo}>PROCURALITICO</Text>
-
-            <View style={estilo.emailSenha}>
-                <TextInput
-                    label='Email'
-                    mode='flat'
-                    left={<TextInput.Icon icon="email" />}
-
-                    onChangeText={(texto) => setEmail(texto)}
-                    defaultValue={email}>
-
-                </TextInput>
-            </View>
-
-            <View style={estilo.emailSenha}>
-
-                <TextInput
-                    label='Senha'
-                    placeholder=""
-                    right={<TextInput.Icon icon="eye" Color="red" />}
-                    left={<TextInput.Icon icon="lock" />}
-                    onChangeText={(texto) => setSenha(texto)}
-                    defaultValue={senha}>
-                </TextInput>
-
-                <View style={estilo.entraVisitCriar}>
-                    <TouchableOpacity onPress={() => navigation.navigate("telaPoliticos")}>
-                        <Text style={estilo.entrarVstt}>Entrar como visitante</Text>
-                    </TouchableOpacity>
-
-                    <TouchableOpacity onPress={() => navigation.navigate("Cadastro")}>
-                        <Text style={estilo.criarCnt}>Criar uma conta</Text>
-                    </TouchableOpacity>
-                </View>
-
-            </View>
-            <TouchableOpacity style={estilo.bordaButao} onPress={() => navigation.navigate("telaPoliticos")}>
-                <Text style={estilo.butao}>LOGIN</Text>
-            </TouchableOpacity>
-
-        </View>
-
->>>>>>> parent of 3c9034ee (tela login pronta)
 
 const TelaLoginComponent = ({ navigation }) => {
   const [email, setEmail] = useState("");
@@ -115,7 +55,6 @@ const TelaLoginComponent = ({ navigation }) => {
 };
 
 const estilo = StyleSheet.create({
-<<<<<<< HEAD
   container: {
     flex: 1,
     justifyContent: "center",
@@ -179,80 +118,3 @@ const estilo = StyleSheet.create({
 });
 
 export default TelaLoginComponent;
-=======
-
-
-
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        backgroundColor: '#45BA5F',
-        height: '100%',
-        width: '100%',
-
-    },
-
-    titulo: {
-        fontSize: 30,
-        color: 'white',
-        fontWeight: 'bold',
-        textAlign: 'center',
-        margin: 0,
-        top: -110
-    },
-
-    title: {
-
-        color: 'white',
-        fontSize: 20,
-        fontWeight: 'bold',
-        marginTop: 28,
-        margin: 13,
-    },
-
-    emailSenha: {
-        width: 393,
-        height: 110,
-        marginLeft: 10,
-        fontSize: 16,
-    },
-
-
-
-    butao: {
-        color: 'white',
-        fontSize: 16,
-        padding: 13,
-        width: 350,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        top: 27,
-        backgroundColor: '#19451F',
-        margin: -10,
-        marginTop: 0
-    },
-    bordaButao: {
-        margin: 26,
-        padding: 20,
-        marginTop: 0
-    },
-    entrarVstt: {
-        color: 'white',
-        width: 145,
-      
-    },
-    criarCnt: {
-        color: 'white',
-        width: 110,
-       
-    },
-    entraVisitCriar:{
-        padding:17,
-        flexDirection:'row',
-        justifyContent:'space-between'
-    }
-})
-
-
-export default TelaLoginComponent;
->>>>>>> parent of 3c9034ee (tela login pronta)
